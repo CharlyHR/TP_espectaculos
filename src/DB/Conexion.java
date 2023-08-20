5e87dcb494a8c0906eaae3102379730da3edfcec
CharlyHR <sebastian.azzolina@gmail.com>
2023-08-20 12:06:48 -0300

    Creada la clase Conexion a la BD

---
+++ src/DB/Conexion.java
@@ -0,0 +1,28 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+	public static final String URL = "jdbc:mysql://localhost:3306/BD_Boleteria";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "";
+	
+	
+	// metodo para conectarse
+	public static Connection getConection() {
+		Connection con = null;
+		try {
+			//Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = (Connection)DriverManager.getConnection(URL,USERNAME,PASSWORD);
+			System.out.println("Conexion Exitosa.");
+		} catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		return con;
+			
+	}
+}
