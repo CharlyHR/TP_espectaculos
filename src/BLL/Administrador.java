649116af89b1ce1897f72d03877b5a3b90acb048
CharlyHR <sebastian.azzolina@gmail.com>
2023-08-21 20:31:12 -0300

    Clase Administrador actualizada, agregado constructor, y metodo CrearEspectaculo, funcional en la BD

--- src/BLL/Administrador.java
+++ src/BLL/Administrador.java
@@ -1,5 +1,19 @@
 package BLL;
 
-public class Administrador {
+import DB.MapperAdministrador;
+
+public class Administrador extends Usuario{
+
+	public Administrador(String nombre, String apellido, int dni, String email, String nombreUsuario, String password) {
+		super(nombre, apellido, dni, email, nombreUsuario, password);
+		// TODO Auto-generated constructor stub
+	}
+	
+	MapperAdministrador mapperAdmin = new MapperAdministrador();
+	public void crearEspectaculo(int id_espectaculo, String nombre, String fecha, int precio, String foto, int capacidadTotal, int capacidadRestante) {
+		
+		mapperAdmin.mapperCrearEspectaculo(id_espectaculo, nombre, fecha, precio, foto, capacidadTotal, capacidadRestante);
+		
+	}
 
 }
