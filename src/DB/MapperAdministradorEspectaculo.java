262858d49bb161416433acc0ac804222c6b4036e
CharlyHR <sebastian.azzolina@gmail.com>
2023-08-21 20:36:57 -0300

    Cambiado el nombre de MapperAdministrador a MapperAdministradorEspectaculo

---
+++ src/DB/MapperAdministradorEspectaculo.java
@@ -0,0 +1,32 @@
+package DB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MapperAdministradorEspectaculo {
+
+	public void mapperCrearEspectaculo(int id_espectaculo, String nombre, String fecha, int precio, String foto, int capacidadTotal, int capacidadRestante) {
+		
+		try {
+			Connection con = null;
+			PreparedStatement ps;
+			ResultSet res;
+			con = Conexion.getConection();
+			
+			Statement s = con.createStatement();
+            String sql = "INSERT INTO espectaculo (id_espectaculo, nombre, fecha, precio, foto, capacidad_total, capacidad_restante ) "
+					+ "				VALUES ('" + id_espectaculo + "', '" + nombre + "', '" + fecha + "', '" + precio + "', '" + foto + "','" + capacidadTotal + "','" + capacidadRestante + "')";
+            s.executeUpdate(sql);
+			
+			con.close(); // la cerre aca por que no me dejaba cerrarla en el finally, no se por que
+		}catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		//place holder
+		System.out.println("Se ha creado un espectaculo:\nNombre: " + nombre);
+		
+	}
+	
+}
