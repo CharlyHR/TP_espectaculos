bb872f9db21523238f5af7255a89d20a0a581a1c
CharlyHR <sebastian.azzolina@gmail.com>
2023-08-23 20:07:30 -0300

    Arreglada sintaxis

--- src/DB/MapperAdministradorUsuario.java
+++ src/DB/MapperAdministradorUsuario.java
@@ -5,23 +5,40 @@ import java.sql.Statement;
 
 public class MapperAdministradorUsuario {
 
-public void mapperCrearUsuario(int dni, String nombre, String apellido, String email, String nombreUsuario, String password) {
+	public void mapperCrearUsuario(int dni, String nombre, String apellido, String email, String nombreUsuario, String password) {
+			
+			try {
+				Connection con = null;
+				con = Conexion.getConection();
+				
+				Statement s = con.createStatement();
+	            String sql = "INSERT INTO usuario (dni, nombre, apellido, email, nombre_usuario, password ) "
+						+ "				VALUES ('" + dni + "', '" + nombre + "', '" + apellido + "', '" + email + "', '" + nombreUsuario + "','" + password + "')";
+	            s.executeUpdate(sql);
+				con.close(); // la cerre aca por que no me dejaba cerrarla en el finally, no se por que
+			}catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			
+			//place holder
+			System.out.println("Se ha creado un usuario:\nNombre: " + nombre);
+			
+		}
+
+	public void mapperEliminarUsuario(int dniUsuario) {
 		
 		try {
 			Connection con = null;
 			con = Conexion.getConection();
-			
 			Statement s = con.createStatement();
-            String sql = "INSERT INTO usuario (dni, nombre, apellido, email, nombre_usuario, password ) "
-					+ "				VALUES ('" + dni + "', '" + nombre + "', '" + apellido + "', '" + email + "', '" + nombreUsuario + "','" + password + "')";
-            s.executeUpdate(sql);
-			con.close(); // la cerre aca por que no me dejaba cerrarla en el finally, no se por que
+			String sql = "DELETE FROM usuario WHERE dni = '" + dniUsuario + "'";
+			s.executeUpdate(sql);
+			con.close(); 
 		}catch (Exception e1) {
 			e1.printStackTrace();
 		}
 		
 		//place holder
-		System.out.println("Se ha creado un usuario:\nNombre: " + nombre);
-		
+		System.out.println("Se ha eliminado el usuario con DNI:\nID: " + dniUsuario);
 	}
 }
